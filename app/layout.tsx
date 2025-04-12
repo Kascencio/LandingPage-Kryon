82e9f7df49b438e0277e0c79ddce99ef6a05f7c3
Kascencio <keduardoascencio@gmail.com>
2025-04-11 22:58:21 -0600

    Update changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -24,6 +26,8 @@ export default function RootLayout({
       <body className={`${inter.className} min-h-screen flex flex-col`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <Header />
+          <Analytics/>
+          <SpeedInsights/>
           {children}
           <Footer />
         </ThemeProvider>
