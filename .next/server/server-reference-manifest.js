61cfb134aa6ed767fd6959469eaa6b6eeea8cc74
Kascencio <keduardoascencio@gmail.com>
2025-04-19 11:25:35 -0600

    Update lasts version for page cabina app

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Rdp9VwGYdYK1jcTT4iixJqPvAYATjFIDzqKqzIRxoko=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
